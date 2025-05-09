3b8a61dd10b2288c94b076b7271e792d27918bb5
Jeff Leyser <jeff@leyser.org>
2025-05-09 21:57:11 +0000

    put back lost env settings for email

--- config/settings.py
+++ config/settings.py
@@ -200,10 +200,11 @@ JWT_AUTH = {
 }
 
 # Email settings
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "your-email@gmail.com"
-EMAIL_HOST_PASSWORD = "your-email-password"
-DEFAULT_FROM_EMAIL = "your-email@gmail.com"
+EMAIL_BACKEND =
+    os.getenv('EMAIL_BACKEND',"django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = os.getenv('EMAIL_HOST', "smtp.gmail.com")
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER',"your-email@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD',"your-email-password")
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL',"your-email@gmail.com")
