d40a4911b1d3d40e70412a25702ae0535554ec84
BrawnaNG <brawna2023@gmail.com>
2025-07-03 20:25:14 +0000

    Add some output as we proceed

--- import/import-all.sh
+++ import/import-all.sh
@@ -15,9 +15,15 @@ fi
 
 rm ../db.sqlite3
 python ../manage.py migrate
+echo "Importing users"
 python wpimport_users.py
+echo -e "\nImporting tags"
 python wpimport_tags.py
+echo -e "\nImporting categories"
 python wpimport_category.py
+echo -e "\nImporting stories"
 python wpimport_story.py
+echo -e "\nConnecting stories and categories"
 python wpimport_connect_story_category.py
+echo -e "\nConnecting stories and tag"
 python wpimport_connect_story_tag.py
